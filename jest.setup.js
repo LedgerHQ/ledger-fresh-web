7f40975d471f06784d678b8b7f23b3caa930cd84
qd-qd <qdqdqdqdqd@protonmail.com>
2023-02-13 12:07:08 +0100

    🔧 configure unit test

---
+++ jest.setup.js
@@ -0,0 +1,5 @@
+// Configure or set up a testing framework before each test.
+
+// Used for __tests__/testing-library.js
+// Learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom/extend-expect";

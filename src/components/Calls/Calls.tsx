2a32bb9069356d6c5d75100953026e7675fa4f59
Thomas Brillard <thomas.brillard@ledger.fr>
2023-06-21 10:55:17 +0200

    Split call Item component

--- src/components/Calls/Calls.tsx
+++ src/components/Calls/Calls.tsx
@@ -15,77 +15,77 @@ const shorten = (data: string) => {
   return data;
 };
 
+type CallProps = {
+  call: Call;
+  index: number;
+};
+
+const CallItem: React.FC<CallProps> = ({ call, index }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  if (call.entrypoint === "approve") {
+    return (
+      <div key={index}>
+        <h4 style={{ display: "flex" }} onClick={() => setIsOpen(!isOpen)}>
+          Action: {call.entrypoint}
+          <Image
+            src="/Icons/chevron-old-down.svg"
+            alt="back"
+            width={16}
+            height={14}
+            priority
+          />
+        </h4>
+        {isOpen && (
+          <div>
+            <div> contract: {shorten(call.contractAddress)}</div>
+            <div>
+              to: {shorten(number.toHex(number.toBN(call?.calldata?.[0] || 0)))}
+            </div>
+            <div>amount: {call.calldata?.[1]?.toString()}</div>
+          </div>
+        )}
+      </div>
+    );
+  } else if (call.entrypoint === "transfer") {
+    return (
+      <div key={index}>
+        <h4>Action: {call.entrypoint}</h4>
+        <div>
+          To {shorten(number.toHex(number.toBN(call?.calldata?.[0] || 0)))}
+        </div>
+        <div>Amount {call.calldata?.[1]?.toString()}</div>
+      </div>
+    );
+  } else {
+    return (
+      <div key={index}>
+        <h4 style={{ display: "flex" }} onClick={() => setIsOpen(!isOpen)}>
+          Action: {call.entrypoint}
+          <Image
+            src="/Icons/chevron-old-down.svg"
+            alt="back"
+            width={16}
+            height={14}
+            priority
+          />
+        </h4>
+        {isOpen && (
+          <ul>
+            {call.calldata?.map((data, i) => (
+              <li key={i}>{shorten(data.toString())}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  }
+};
 export function Calls({ calls, ...props }: Props) {
   return (
     <>
-      {calls.map((call, index) => {
-        const [isOpen, setIsOpen] = useState(false);
-
-        console.log(call);
-        if (call.entrypoint === "approve") {
-          return (
-            <div key={index}>
-              <h4 style={{ display: "flex" }}>
-                Action: {call.entrypoint}
-                <Image
-                  src="/Icons/chevron-old-down.svg"
-                  alt="back"
-                  width={16}
-                  height={14}
-                  priority
-                  onClick={() => setIsOpen(!isOpen)}
-                />
-              </h4>
-              {isOpen && (
-                <div>
-                  <div> contract: {shorten(call.contractAddress)}</div>
-                  <div>
-                    to:{" "}
-                    {shorten(
-                      number.toHex(number.toBN(call?.calldata?.[0] || 0))
-                    )}
-                  </div>
-                  <div>amount: {call.calldata?.[1]?.toString()}</div>
-                </div>
-              )}
-            </div>
-          );
-        } else if (call.entrypoint === "transfer") {
-          return (
-            <div key={index}>
-              <h4>Action: {call.entrypoint}</h4>
-              <div>
-                To{" "}
-                {shorten(number.toHex(number.toBN(call?.calldata?.[0] || 0)))}
-              </div>
-              <div>Amount {call.calldata?.[1]?.toString()}</div>
-            </div>
-          );
-        } else {
-          return (
-            <div key={index}>
-              <h4 style={{ display: "flex" }}>
-                Action: {call.entrypoint}
-                <Image
-                  src="/Icons/chevron-old-down.svg"
-                  alt="back"
-                  width={16}
-                  height={14}
-                  priority
-                  onClick={() => setIsOpen(!isOpen)}
-                />
-              </h4>
-              {isOpen && (
-                <ul>
-                  {call.calldata?.map((data, i) => (
-                    <li key={i}>{shorten(data.toString())}</li>
-                  ))}
-                </ul>
-              )}
-            </div>
-          );
-        }
-      })}
+      {calls.map((call, index) => (
+        <CallItem call={call} index={index} key={index} />
+      ))}
     </>
   );
 }

5b26dbe6144b84c7e5355756e582e8873fb6339c
Thomas Brillard <thomas.brillard@ledger.fr>
2023-07-10 20:38:08 +0200

    balanceChanges WIP

--- src/pages/send/index.tsx
+++ src/pages/send/index.tsx
@@ -68,7 +68,7 @@ export default function Send() {
         call,
         invoDetails,
         account.authenticatorId,
-        starkCheckSignature
+        starkCheckSignature.signature
       );
 
       addTransaction({

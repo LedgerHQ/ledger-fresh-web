c214327516b610439c7a3f134fb187e02089af45
Thomas Brillard <thomas.brillard@ledger.fr>
2023-07-16 12:06:13 +0200

    display transaction error

--- src/components/Starkcheck/Starkcheck.tsx
+++ src/components/Starkcheck/Starkcheck.tsx
@@ -2,12 +2,27 @@ import styles from "./Starkcheck.module.css";
 
 interface Props {
   checked: boolean;
-  error: string;
+  error: string | { errorCode: string };
 }
 
 export function Starkcheck({ checked, error, ...props }: Props) {
   if (error) {
     console.log(error);
+    console.log(typeof error !== "string" && error.errorCode);
+    if (typeof error !== "string" && error.errorCode) {
+      switch (error.errorCode) {
+        case "StarknetErrorCode.TRANSACTION_FAILED":
+          return (
+            <div className={styles.row}> ❌ Transaction will fail ❌ </div>
+          );
+
+        default:
+          return (
+            <div className={styles.row}>Unknown error {error.errorCode}</div>
+          );
+      }
+    }
+
     return <div className={styles.row}>Starkcheck error ❌</div>;
   }
   if (!checked) {
